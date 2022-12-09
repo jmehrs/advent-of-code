5e7c3e4a2f687d4214aeb8d2b245befcc8781442
Jai Mehra <jmjaiken@gmail.com>
2022-12-08 21:08:05 -0500

    some refactoring of day 5 part-1 code & completion of day 5 part-2

--- 2022/day-05/src/lib.rs
+++ 2022/day-05/src/lib.rs
@@ -1,42 +1,56 @@
 mod parse;
 
-use std::{collections::{HashMap, VecDeque}, slice::SliceIndex};
+use std::collections::{HashMap, VecDeque};
 
 pub use parse::{parse_input_part1, parse_input_part2, Instructions};
 
-fn follow_instruction(vec_map: &mut HashMap<u8, VecDeque<char>>, instruction: &Instructions) {
-    if let Some(value) = vec_map.get_mut(&instruction.src).unwrap().pop_front() {
-        vec_map.get_mut(&instruction.dst).unwrap().push_front(value);
+fn follow_instruction_part1(vec_map: &mut HashMap<u8, VecDeque<char>>, instruction: &Instructions) {
+    for _ in 0..instruction.amount {
+        if let Some(value) = vec_map.get_mut(&instruction.src).unwrap().pop_front() {
+            vec_map.get_mut(&instruction.dst).unwrap().push_front(value);
+        }
     }
 }
 
-fn follow_instructions(vec_map: &mut HashMap<u8, VecDeque<char>>, instructions: &Vec<Instructions>) {
-    for instruction in instructions {
-        for _ in 0..instruction.amount {
-            follow_instruction(vec_map, instruction)
+fn follow_instruction_part2(vec_map: &mut HashMap<u8, VecDeque<char>>, instruction: &Instructions) {
+    for i in 0..instruction.amount{
+        if let Some(value) = vec_map.get_mut(&instruction.src).unwrap().pop_front() {
+            vec_map.get_mut(&instruction.dst).unwrap().insert(i as usize, value);
         }
+    }
+}
+
+fn follow_instructions<Spec>(
+    vec_map: &mut HashMap<u8, VecDeque<char>>,
+    instructions: &Vec<Instructions>,
+    specification: Spec
+)
+where
+    Spec: Fn(&mut HashMap<u8, VecDeque<char>>, &Instructions)
+{
+    for instruction in instructions {
+        specification(vec_map, &instruction)
     }
 }
 
 fn get_vec_ends(vec_map: &HashMap<u8, VecDeque<char>>) -> String {
     (1..=vec_map.len())
-        .map(|idx| {
-            vec_map.get(&(idx as u8)).unwrap()[0]
-        })
+        .map(|idx| vec_map.get(&(idx as u8)).unwrap()[0])
         .collect()
 }
 
 pub fn part1_solution1(input: &str) -> String {
     let (mut vec_map, instructions) = parse_input_part1(input);
-    follow_instructions(&mut vec_map, &instructions);
+    follow_instructions(&mut vec_map, &instructions, follow_instruction_part1);
     let result = get_vec_ends(&vec_map);
     result
 }
 
 pub fn part2_solution1(input: &str) -> String {
-    let input = parse_input_part2(input);
-    let result = input;
-    format!("{result:?}")
+    let (mut vec_map, instructions) = parse_input_part1(input);
+    follow_instructions(&mut vec_map, &instructions, follow_instruction_part2);
+    let result = get_vec_ends(&vec_map);
+    result
 }
 
 #[cfg(test)]
@@ -62,6 +76,6 @@ move 1 from 1 to 2";
     #[test]
     fn p2s1() {
         let result = part2_solution1(INPUT);
-        assert_eq!(result, "70");
+        assert_eq!(result, "MCD");
     }
 }
